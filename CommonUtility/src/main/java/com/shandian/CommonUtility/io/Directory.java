5f10396d293b2e8faf9d904dd2e94646dcad05a4
shandian534 <985346375@sina.com>
2017-07-02 15:36:01 +0800

    add directory.java

---
+++ CommonUtility/src/main/java/com/shandian/CommonUtility/io/Directory.java
@@ -0,0 +1,98 @@
+package com.shandian.CommonUtility.io;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public final class Directory {
+    public static File[] local(File dir, final String regex) {
+        return dir.listFiles(new FilenameFilter() {
+            private final Pattern pattern = Pattern.compile(regex);
+
+            public boolean accept(File dir, String name) {
+                return pattern.matcher(new File(name).getName()).matches();
+            }
+        });
+    }
+
+    public static File[] local(String path, final String regex) {
+        return local(new File(path), regex);
+    }
+
+    // ?这个类应该是有问题的
+    public static class TreeInfo implements Iterator<File> {
+        public List<File> files = new ArrayList<File>();
+        public List<File> dirs = new ArrayList<File>();
+
+        public Iterator<File> iterator() {
+            return files.iterator();
+        }
+
+        void addAll(TreeInfo other) {
+            files.addAll(other.files);
+            dirs.addAll(other.dirs);
+        }
+
+        @Override
+        public String toString() {
+            return "dirs:" + dirs.toString() + "\n\n files" + files.toString();
+        }
+
+        public boolean hasNext() {
+            // TODO Auto-generated method stub
+            return false;
+        }
+
+        public File next() {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        public void remove() {
+            // TODO Auto-generated method stub
+        }
+    }
+
+    public static TreeInfo walk(String start, String regex) {
+        return recurseDirs(new File(start), regex);
+    }
+
+    public static TreeInfo walk(File start, String regex) {
+        return recurseDirs(start, regex);
+    }
+
+    public static TreeInfo walk(File start) {
+        return recurseDirs(start, ".*");
+    }
+
+    public static TreeInfo walk(String start) {
+        return recurseDirs(new File(start), ".*");
+    }
+
+    static TreeInfo recurseDirs(File startDir, String regex) {
+        TreeInfo result = new TreeInfo();
+        for (File item : startDir.listFiles()) {
+            if (item.isDirectory()) {
+                result.dirs.add(item);
+                result.addAll(recurseDirs(item, regex));
+            } else {
+                if (item.getName().matches(regex)) {
+                    result.files.add(item);
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println(walk("."));
+        } else
+            for (String arg : args) {
+                System.out.println(walk(arg));
+            }
+    }
+}
